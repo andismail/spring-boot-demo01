94bf5efdd7cc0d54ae6855a9c3fae76d222c2f74
Jthan <andismail@163.com>
2017-08-15 19:05:18 +0800

    hello world & run spring-boot

--- src/main/java/com/example/GirlApplication.java
+++ src/main/java/com/example/GirlApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GirlApplication {
 
+	//启动方式一:直接在IDE中run
+    //启动方式二:mvn package 后 java -jar girl-0.0.1-SNAPSHOT.jar
+    //启动方式三:mvn spring-boot:run
+    //注意:后两种启动方式需确保运行环境的java版本
 	public static void main(String[] args) {
 		SpringApplication.run(GirlApplication.class, args);
 	}
